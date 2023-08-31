8e5eca3b3c446108145830b39f55dd5df8b7d090
yoga.qf <yoga.qf@gmail.com>
2023-08-31 16:14:41 +0700

    OK

--- typing.js
+++
@@ -1,6 +0,0 @@
-var typed = new Typed(".typing", {
-      strings: ["Masjid", "Kolam Renang", "Lapangan Tenis", "Ruang Meeting", "Restoran"],
-      typeSpeed: 50,
-      backSpeed: 60,
-      loop: true
-});
